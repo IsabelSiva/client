1000acf808ee6e0f264a30b181fae925aa9bd0f5
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2023-09-27 13:54:25 -0300

    fix GET endpoints into clients controller and add a post method

--- src/main/java/com/isabelSilva/client/controllers/ClientController.java
+++ src/main/java/com/isabelSilva/client/controllers/ClientController.java
@@ -3,11 +3,14 @@ package com.isabelSilva.client.controllers;
 import com.isabelSilva.client.dto.ClientDTO;
 import com.isabelSilva.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,13 +21,34 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping
-    public List<ClientDTO> findAll(){
-        return this.service.findAll();
+    public ResponseEntity<Page<ClientDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy){
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction),orderBy );
+        Page<ClientDTO> list = service.findAll(pageRequest);
+
+        return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ClientDTO findOne(@PathVariable Long id){
-        return this.service.findOne(id);
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO ProductDTO = service.findById(id);
+
+        return ResponseEntity.ok().body(ProductDTO);
     }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
 
 }
--- src/main/java/com/isabelSilva/client/services/ClientService.java
+++ src/main/java/com/isabelSilva/client/services/ClientService.java
@@ -4,11 +4,10 @@ import com.isabelSilva.client.dto.ClientDTO;
 import com.isabelSilva.client.entities.Client;
 import com.isabelSilva.client.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -16,16 +15,32 @@ public class ClientService {
     private ClientRepository repository;
 
     @Transactional(readOnly = true)
-    public List<ClientDTO> findAll(){
-        List<Client> clientList = this.repository.findAll();
-        List<ClientDTO> dtoList = clientList.stream().map(e -> new ClientDTO(e)).collect(Collectors.toList());
-        return dtoList;
+    public Page<ClientDTO> findAll(PageRequest pageRequest){
+        Page<Client> categoryList = repository.findAll(pageRequest);
+        return categoryList.map(ClientDTO::new);
     }
 
     @Transactional(readOnly = true)
-    public ClientDTO findOne(Long id) {
+    public ClientDTO findById(Long id) {
         Client client = this.repository.getById(id);
         ClientDTO clientDTO = new ClientDTO(client);
         return  clientDTO;
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client client = new Client();
+        copyDtoToEntity(dto, client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client client) {
+        client.setId(dto.getId());
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
     }
 }
